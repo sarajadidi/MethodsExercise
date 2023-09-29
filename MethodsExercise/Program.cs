5850009a25fcce23b8e1a6d383f924a1e2a0f8fb
sarajadidi <sararivkajadidi@gmail.com>
2023-09-28 21:44:14 -0700

    Completed exercise #1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}!");
+
+            Console.WriteLine("What is your favorite color?");
+
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Animal?");
+
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Band?");
+
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"Once upon a time, {name}'s {color} {animal} " +
+                $"went for a run out in the wilderness. " +
+                $"All of a sudden, {name}'s {animal} found itself lost! " +
+                $"But, they followed the music of {band}, and found their way home!");
+
+            Console.WriteLine("Thanks for playing!:)");
+
+
+               
         }
     }
 }
